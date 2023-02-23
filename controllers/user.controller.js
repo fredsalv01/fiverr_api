ad54ecfc285303b25813ffefe7349c9e55a50858
fredalbert37 <fredalbert37@outlook.com>
2023-02-23 06:17:03 -0500

    delete user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,17 @@
-export const fn = (req, res) => {
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export const deleteUser = async (req, res) => {
   // TODO: implement
-  res.send('deleteUser');
-}
+  const user = await User.findById(req.params.id);
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).send("Unauthenticated");
+  jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+    if (payload.id === user._id)
+      res.status(403).send("You can delete only your account user");
+    await User.findByIdAndDelete(req.params.id);
+    res.send("User deleted");
+  });
+};
+
+export const fn = (req, res) => {};
